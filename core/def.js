da7b1bc73b4728c3ba8f0b79bd09638cd79e5eb4
ebgamer4438 <89718796+ebgamer4438@users.noreply.github.com>
2022-03-25 07:57:52 +0430

    Add files via upload

---
+++ core/def.js
@@ -0,0 +1,6 @@
+var imgs_list = ["1.png","2.png",]
+var position = "left"
+var size = 300
+var border = 0
+var time_stay = 5
+var time_out = 1
---
+++ core/slider.js
@@ -0,0 +1,48 @@
+//_______________________________________________________________
+//here codes dont touch thatඞ
+var counter = 0
+var index_size = imgs_list.length - 1
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+document.getElementById("img1").className = position;
+if(position == "top" || position == "bottom"){
+    document.getElementsByTagName("BODY")[0].style = "display: flex; justify-content: center;";
+}
+else{
+    document.getElementById("img1").style = "position: fixed;"
+}
+document.getElementById("img1").style.width = size+"px";
+document.getElementById("img1").style.borderRadius = border + "em";
+
+function set_img(){
+    document.getElementById("img1").src="pics/" + imgs_list[counter];
+    if(counter != index_size){
+       counter = counter + 1
+    }
+    else{
+       counter = 0
+    }
+    
+}
+set_img()
+
+function enter(){
+    document.getElementById("img1").style.animation = 'fadeInAnimation-' + position + ' forwards 1s'
+}
+function exit(){
+    document.getElementById("img1").style.animation = 'fadeOutAnimation-' + position + ' forwards 1s'
+}
+
+async function run(){
+    while (true){
+        enter()
+        await sleep(time_stay * 1000)
+        exit()
+        await sleep(time_out * 1000)
+        set_img()
+    }
+}
+
+run()
